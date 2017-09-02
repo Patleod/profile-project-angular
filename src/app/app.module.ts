cad9b2d63c9028e97d219509b56438be33fcb77d
patleod <nomadepat@gmail.com>
2017-09-02 08:50:06 +0200

    DZone Creating a Front-End for Your User Profile Store With Angular and TypeScript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogComponent } from './blog/blog.component';
+
+let routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "blogs", component: BlogsComponent },
+  { path: "blog", component: BlogComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BlogsComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
